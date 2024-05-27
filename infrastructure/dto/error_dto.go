591d51e05023d78ed6d479627252dbc6c52c3442
Zorica Vukovic <zoka200015@gmail.com>
2024-05-27 16:19:43 +0200

    da

--- infrastructure/dto/error_dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type ErrorResponse struct {
-	Message    string `json:"message"`
-	StatusCode int    `json:"statusCode"`
-}
--- infrastructure/dto/verification_dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type VerificationDTO struct {
-	Id     string `json:"verificationId"`
-	UserId string `json:"userId"`
-}
